422e8e14edefdbcfeb6679b01d731e3bf7f5b142
irshad Ahmed <iaschowrai@gmail.com>
2022-11-16 21:09:35 -0500

    updated version

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,38 +2,35 @@ package com.company;
 
 import javax.swing.*;
 
-
 public class Main {
-
     public static void main(String[] args) {
 
-        int computerNumber = (int) (Math.random() * 100 + 1);
-        int userAnswer = 0;
+        int randomNumber = (int) (Math.random() * 10 + 1);
+        int userInput = 0;
 
-        System.out.println("The correct guess would be " + computerNumber);
+        //System.out.println("The correct guess would be " + randomNumber);
 
         int count = 1;
-        while (userAnswer != computerNumber) {
-            String response = JOptionPane.showInputDialog(null, "Guess a Number between 1 to 100", "Guessing Number", 3);
-            userAnswer = Integer.parseInt(response);
-            JOptionPane.showMessageDialog(null, "" + determineGuess(userAnswer, computerNumber, count));
+        while (userInput != randomNumber) {
+            String response = JOptionPane.showInputDialog(null, "Guess a Number between 1 to 10", "Guessing Number", 3);
+            userInput = Integer.parseInt(response);
+            JOptionPane.showMessageDialog(null, "" + determineGuess(userInput, randomNumber, count));
 
             count++;
         }
     }
 
-    public static String determineGuess(int userAnswer, int computerNumber, int count){
-        if(userAnswer <= 0 || userAnswer>100){
+    public static String determineGuess(int userInput, int randomNumber, int count){
+        if(userInput <= 0 || userInput>10){
             return "Your guess is invalid";
-
         }
-        else if (userAnswer == computerNumber){
+        else if (userInput == randomNumber){
             return "Correct! \n Total Guesses: " + count;
         }
-        else if (userAnswer < computerNumber){
-            return "Your guess is too low, try again.\n Try Number: "+ count;
+        else if (userInput < randomNumber){
+            return "Your guess is too low, try again.\nTry Number: "+ count;
         }
-        else if(userAnswer > computerNumber){
+        else if(userInput > randomNumber){
             return "Your guess too high, try again.\nTry Number: "+ count;
         }
         else{
